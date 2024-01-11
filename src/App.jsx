14a4652be9d602fe3de4965ff052d182cf6f3da1
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2024-01-11 15:20:59 +0400

    work with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Jumbotron from "./components/jumbotron";
+import JumboData from "./fixtures/jumbo.json";
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Jumbotron.Container>
+      {JumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane></Jumbotron.Pane>
+          <Jumbotron.Image src={item.image} alt={item.alt} />
+          <p>{item.alt}</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
